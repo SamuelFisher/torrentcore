d06147e53ff406c61b6491daf76593990a0c4140
Carlos Jiménez Delgado <cjdelgado@plainconcepts.com>
2019-12-19 19:00:13 +0100

    Peer exchange extension implementation

---
+++ TorrentCore/Extensions/PeerExchange/PeerExchangeMetadata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TorrentCore.Extensions.PeerExchange
+{
+    public class PeerExchangeMetadata
+    {
+        public static string Key => "PeerExchange::Metadata";
+
+        public DateTime LastMessageDate { get; set; }
+
+        public IEnumerable<string> ConnectedPeersSnapshot { get; set; } = new List<string>();
+    }
+}
